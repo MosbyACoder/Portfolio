575ce538610f1e5cebaa63c8e78d5be1c5a05d24
MosbyACoder <lfrase15@my.centennialcollege.ca>
2024-10-01 02:34:14 -0400

    create project page

---
+++ client/src/project.jsx
@@ -0,0 +1,17 @@
+
+export default function Education(){
+    return (
+        <div>
+            <h2>Projects page</h2>
+            <br />
+            <p>Project 1: educational application for learning langauges.</p>
+            <p>closest app I have found is Mondly by Pearson. App can be used in virtual reality with meta quest vr headsets.</p>
+            <br />
+            <p>Project 2: connecting multiple computers</p>
+            <p>synergy app is closest I've found to the idea. uses one mouse and one keyboard between several computers. Can't share computing resources but can copy and paste between systems.</p>
+            <br />
+            <p>Project 3: use Ipad as drawing tablet</p>
+            <p>project blue is closest I've found to the idea. Essentially uses Ipad as a spare monitor.</p>
+        </div>
+    )
+}
