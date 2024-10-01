fd197a82dc854064f3724381af2794e0882bdec3
MosbyACoder <lfrase15@my.centennialcollege.ca>
2024-10-01 04:12:04 -0400

    update line taken out of about page

--- client/src/about.jsx
+++ client/src/about.jsx
@@ -2,8 +2,7 @@
 export default function About(){
     return (
         <div>
-            <h1>About Me Page</h1>
-            <h1>Lucas Fraser's assignment 1</h1>
+            <h1>About Me Page</h1>            
             <p>Student of Web Application Development</p>
             <p>at Centennial College</p>
             <p>for Software Engineering - Artificial Intelligence</p>
