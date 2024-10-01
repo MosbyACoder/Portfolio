32171383cd58f7b4fdf7c5eefab703cbdb8e2c8b
MosbyACoder <lfrase15@my.centennialcollege.ca>
2024-10-01 18:42:13 -0400

    added picture to about page

--- client/src/about.jsx
+++ client/src/about.jsx
@@ -1,8 +1,18 @@
+import selfpic from '../src/assets/selfpicture.jpg'
+import '../src/about.css'
 
 export default function About(){
     return (
         <>
         <div class="wrapper">
+            <div class="box">
+                <div class="pic">
+                    <img src={selfpic} alt="self portrait"></img>
+                </div>
+                <div class="txt">
+                    Lucas Fraser
+                </div>
+            </div>
             <h1>About Me Page</h1>            
             <p>Student of Web Application Development</p>
             <p>at Centennial College</p>
