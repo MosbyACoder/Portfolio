2204fcf6953b17a45a9a4f5694cbbd502fde5d4b
MosbyACoder <lfrase15@my.centennialcollege.ca>
2024-10-01 02:11:33 -0400

    create contact page

---
+++ client/src/contact.jsx
@@ -0,0 +1,11 @@
+
+export default function Contact() {
+    return (
+        <div>
+            <p>Contact page</p>
+            <h3>Lucas Fraser</h3>
+            <p>phone: 613-485-6549</p>
+            <p>email: lfrase15@my.centennialcollege.ca</p>
+        </div>
+    )
+}
