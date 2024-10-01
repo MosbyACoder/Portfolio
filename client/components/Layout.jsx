ee7137df43ed102f24ccef26f321de13e57391b8
MosbyACoder <lfrase15@my.centennialcollege.ca>
2024-10-01 02:58:26 -0400

    update layout page

---
+++ client/components/Layout.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Layout() {
+    return (
+        <div>
+            <h1>Lucas Fraser's Portfolio</h1>
+            <nav>
+                <Link to="/">Home</Link> |
+                <Link to="/about">About</Link> |
+                <Link to="/project">Project</Link> |
+                <Link to="/contact">Contact</Link>
+            </nav>
+            <hr />
+        </div>
+    );
+}
