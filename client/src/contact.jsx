54a167d6bbd02d779f3c6e50fcb7df1407e0e711
MosbyACoder <lfrase15@my.centennialcollege.ca>
2024-10-01 03:59:47 -0400

    update header in contact page

--- client/src/contact.jsx
+++ client/src/contact.jsx
@@ -2,7 +2,7 @@
 export default function Contact() {
     return (
         <div>
-            <p>Contact page</p>
+            <h1>Contact page</h1>
             <h3>Lucas Fraser</h3>
             <p>phone: 613-485-6549</p>
             <p>email: lfrase15@my.centennialcollege.ca</p>
