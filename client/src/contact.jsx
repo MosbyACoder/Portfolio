fd688fba258c84cb2884cac732067734862f8299
MosbyACoder <lfrase15@my.centennialcollege.ca>
2024-10-01 20:48:43 -0400

    update contact page

--- client/src/contact.jsx
+++ client/src/contact.jsx
@@ -1,14 +1,22 @@
+import selfpic from '../src/assets/selfpicture.jpg'
 
 export default function Contact() {
     return (
         <>
         <div class="wrapper">
-            
-
             <h1>Contact page</h1>
-            <h3>Lucas Fraser</h3>
-            <p>phone: 613-485-6549</p>
-            <p>email: lfrase15@my.centennialcollege.ca</p>
+            <div class="box">
+                <div class="pic">
+                    <img src={selfpic} alt="mondly languages" width="100" height="100"></img>
+                </div>
+                <div class="txt">
+                    <h3>Lucas Fraser</h3>
+                    <ul>
+                        <li>phone: 613-485-6549</li>
+                        <li>email: lfrase15@my.centennialcollege.ca</li>
+                    </ul>
+                </div>
+            </div>
         </div>
         </>
     );
